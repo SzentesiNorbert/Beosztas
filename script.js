d7e4a7d47a3360d6e890a564a93f0b94680e2d86
Szentesi Norbert <39724754+SzentesiNorbert@users.noreply.github.com>
2018-08-26 14:32:01 +0200

    Szumma felületek és JS próbálkozások

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function ujDolg(){
   var vnev = document.getElementById('vnev').value;
   var knev = document.getElementById('knev').value;
   var table = document.getElementsByTagName('table')[0];
+  var table2 = document.getElementsByTagName('table')[1];
   var dolgozo = table.insertRow(0);
+  var dolgozo2 = table.insertRow(1);
   var cell1 = dolgozo.insertCell(0);
   var cell2 = dolgozo.insertCell(1);
   cell1.innerHTML = vnev;
@@ -13,33 +15,24 @@ function ujDolg(){
 
 
 /*Munkaóra kiszámítás*/
+var Munka = 11;
+function myInputM(){
 
-var munkaOra = 0;
-var pi = 0;
-var sz =0;
+ Munka + 11;
+console.log(Munka);
+};
 
+function munkaOra() {
+  cell.innerHTML = dolgozo1;
 
+return myInputM();
 
-  function myInputM(){
-
-    var mn = document.getElementById('myInput')
-    var pi = document.getElementById('myInputP')
-    var sz = document.getElementById('myInputSZ1')
-    var b = document.getElementById('myInputB1')
-  if (mn.clicked=true) {
-    munkaOra =+11;
-    pi = 0;
-    sz = 0;
-}
-console.log(munkaOra);
-console.log(sz);
 
 }
-
 /*Munkaidő korrigálás*/
 
 var korr = document.getElementById('myInputKorr1')
 function fix(){
-  var munkaOra = munkaOra-korr
+  var Munka = Munka-korr;
 }
 console.log(munkaOra);
