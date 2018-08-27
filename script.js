28a4cd7aeea7a43e7888786e44e56475746466b4
Szentesi Norbert <39724754+SzentesiNorbert@users.noreply.github.com>
2018-08-28 00:26:46 +0200

    Munkaóra és Szabadság számítás

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function ujDolg(){
   var table = document.getElementsByTagName('table')[0];
   var table2 = document.getElementsByTagName('table')[1];
   var dolgozo = table.insertRow(0);
-  var dolgozo2 = table.insertRow(1);
   var cell1 = dolgozo.insertCell(0);
   var cell2 = dolgozo.insertCell(1);
   cell1.innerHTML = vnev;
@@ -16,13 +15,160 @@ function ujDolg(){
 
 /*Munkaóra kiszámítás*/
 var Munka=0;
-function myInputM()
+var Munka_b=0;
+var Munka_c=0;
+var Munka_d=0;
+cella=[];
+cella2=[];
+cella3=[];
+cella4=[];
+//cella[0]=false;
+function myInputM(x)
 {
- this.Munka = Munka + 11;
-console.log(this.Munka);
+  if (cella[x]==undefined) {
+    cella[x]=false;
+  }
+  if (cella[x]==false) {
+    this.Munka += 11;
+    cella[x]=true;
+  }
+  console.log(this.Munka);
+};
+
+  function myInputM_b(y)
+  {
+    if (cella2[y]==undefined) {
+      cella2[y]=false;
+    }
+    if (cella2[y]==false) {
+      this.Munka_b += 11;
+      cella2[y]=true;
+    }
+
+console.log(this.Munka_b);
 
 };
 
+function myInputM_c(z)
+{
+  if (cella3[z]==undefined) {
+    cella3[z]=false;
+  }
+  if (cella3[z]==false) {
+    this.Munka_c += 11;
+    cella3[z]=true;
+  }
+
+console.log(this.Munka_c);
+
+};
+
+function myInputM_d(zz)
+{
+  if (cella4[zz]==undefined) {
+    cella4[zz]=false;
+  }
+  if (cella4[zz]==false) {
+    this.Munka_d += 11;
+    cella4[zz]=true;
+  }
+
+console.log(this.Munka_d,);
+
+};
+
+/*Szabadság kiszámítás*/
+
+var Szabi=0;
+var Szabi_b=0;
+var Szabi_c=0;
+var Szabi_d=0;
+cellaSz = [];
+cellaSz2 = [];
+cellaSz3 = [];
+cellaSz4 = [];
+
+//cella[0]=false;
+function myInputSz(x)
+{
+  if (cellaSz[x]==undefined) {
+    cellaSz[x]=false;
+  }
+  if (cellaSz[x]==false) {
+    this.Szabi += 8;
+    cellaSz[x]=true;
+  }
+
+
+console.log(this.Szabi);
+
+};
+
+function myInputSz_b(y)
+{
+  if (cellaSz2[y]==undefined) {
+    cellaSz2[y]=false;
+  }
+  if (cellaSz2[y]==false) {
+    this.Szabi_b += 8;
+    cellaSz2[y]=true;
+  }
+
+
+console.log(this.Szabi_b);
+
+};
+
+function myInputSz_c(z)
+{
+  if (cellaSz3[z]==undefined) {
+    cellaSz3[z]=false;
+  }
+  if (cellaSz3[z]==false) {
+    this.Szabi_c += 8;
+    cellaSz3[z]=true;
+  }
+
+
+console.log(this.Szabi_c);
+
+};
+
+function myInputSz_d(zz)
+{
+  if (cellaSz4[zz]==undefined) {
+    cellaSz4[zz]=false;
+  }
+  if (cellaSz4[zz]==false) {
+    this.Szabi_d += 8;
+    cellaSz4[zz]=true;
+  }
+
+
+console.log(this.Szabi_d);
+
+};
+
+/*Betegség kiszámítás*/
+/*
+var BSzabi=0;
+cella = [];
+//cella[0]=false;
+function myInputB(x)
+{
+  if (cella[x]==undefined) {
+    cella[x]=false;
+  }
+  if (cella[x]==false) {
+    Munka - 3;
+    cella[x]=true;
+  }
+
+
+console.log(this.Munka, cella[x]);
+
+
+};
 function munkaOra() {
   cell.innerHTML = dolgozo1.this.Munka;
 
@@ -31,9 +177,10 @@ return myInputM();
 
 }
 /*Munkaidő korrigálás*/
-
+/*
 var korr = document.getElementById('myInputKorr1')
 function fix(){
   var Munka = Munka-korr;
 }
 console.log(munkaOra);
+*/
